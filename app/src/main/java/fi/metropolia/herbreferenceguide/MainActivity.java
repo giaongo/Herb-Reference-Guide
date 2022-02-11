a6afcf63e391890a8d94e8bc00d3ec8df9d6d846
Giao Ngo <giao.ngo@metropolia.fi>
2022-02-11 18:32:56 +0200

    Add onclick event listeners for every card views in MainActivity

--- app/src/main/java/fi/metropolia/herbreferenceguide/MainActivity.java
+++ app/src/main/java/fi/metropolia/herbreferenceguide/MainActivity.java
@@ -3,12 +3,40 @@ package fi.metropolia.herbreferenceguide;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.card.MaterialCardView;
 
 public class MainActivity extends AppCompatActivity {
-
+    MaterialCardView mcHerbs, mcFruits, mcVegetables, mcNote;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CardClick cardClick = new CardClick();
+        mcHerbs = findViewById(R.id.mcHerbs);
+        mcFruits = findViewById(R.id.mcFruits);
+        mcVegetables =  findViewById(R.id.mcVegetables);
+        mcNote = findViewById(R.id.mcNotes);
+        mcHerbs.setOnClickListener(cardClick);
+        mcFruits.setOnClickListener(cardClick);
+        mcVegetables.setOnClickListener(cardClick);
+        mcNote.setOnClickListener(cardClick);
+    }
+
+    private class CardClick  implements View.OnClickListener  {
+        @Override
+        public void onClick(View view) {
+            if(view.getId() == R.id.mcHerbs) {
+                Toast.makeText(MainActivity.this, "Herb clicked", Toast.LENGTH_SHORT).show();
+            } else if(view.getId() == R.id.mcFruits) {
+                Toast.makeText(MainActivity.this, "Fruit clicked", Toast.LENGTH_SHORT).show();
+            } else if(view.getId() == R.id.mcVegetables) {
+                Toast.makeText(MainActivity.this, "Vegetable clicked", Toast.LENGTH_SHORT).show();
+            } else if(view.getId() == R.id.mcNotes)  {
+                Toast.makeText(MainActivity.this, "Note clicked", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
