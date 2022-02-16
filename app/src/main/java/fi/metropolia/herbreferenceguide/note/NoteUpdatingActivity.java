cd3ee40dbb1351b4be4bdd290dd8ba8aeacdc9cc
Giao Ngo <giao.ngo@metropolia.fi>
2022-02-16 18:22:17 +0200

    Adding delete functions for delete buttons and fix warnings in NoteUpdatingActivity. Contribute to #3

--- app/src/main/java/fi/metropolia/herbreferenceguide/note/NoteUpdatingActivity.java
+++ app/src/main/java/fi/metropolia/herbreferenceguide/note/NoteUpdatingActivity.java
@@ -1,7 +1,6 @@
 package fi.metropolia.herbreferenceguide.note;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
@@ -10,26 +9,30 @@ import android.widget.Toast;
 
 import fi.metropolia.herbreferenceguide.R;
 import fi.metropolia.herbreferenceguide.database.AppDatabase;
+import fi.metropolia.herbreferenceguide.database.Note;
 
 public class NoteUpdatingActivity extends AppCompatActivity {
-    Button btnUpdate, btnDelete;
-    EditText titleUpdate, descriptionUpdate;
-    int notePosition;
+    private EditText titleUpdate, descriptionUpdate;
+    private int notePosition;
+    private String title, description;
+    private AppDatabase database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_updating);
         loadData();
-        btnUpdate = findViewById(R.id.btnUpdate);
+        database = AppDatabase.getINSTANCE(NoteUpdatingActivity.this);
+        Button btnUpdate = findViewById(R.id.btnUpdate);
+        Button btnDelete = findViewById(R.id.btnDelete);
         btnUpdate.setOnClickListener(view -> updateNoteToDatabase());
-
+        btnDelete.setOnClickListener(view -> deleteNoteFromDatabase());
     }
 
     private void loadData() {
         Intent intent = getIntent();
         notePosition = intent.getIntExtra(NoteActivity.NOTE_POSITION,0);
-        String title = intent.getStringExtra(NoteActivity.TITLE_UPDATE);
-        String description = intent.getStringExtra(NoteActivity.DESCRIPTION_UPDATE);
+        title = intent.getStringExtra(NoteActivity.TITLE_UPDATE);
+        description = intent.getStringExtra(NoteActivity.DESCRIPTION_UPDATE);
 
         titleUpdate = findViewById(R.id.edtTitleUpdate);
         descriptionUpdate = findViewById(R.id.edtDescriptionUpdate);
@@ -40,12 +43,16 @@ public class NoteUpdatingActivity extends AppCompatActivity {
     private void updateNoteToDatabase() {
         String updatedTitle = titleUpdate.getText().toString();
         String updatedDescription = descriptionUpdate.getText().toString();
-        AppDatabase database = AppDatabase.getINSTANCE(NoteUpdatingActivity.this);
-        int result = database.noteDao().updateNote(updatedTitle, updatedDescription, notePosition);
-        Toast.makeText(NoteUpdatingActivity.this, "Update : " + result, Toast.LENGTH_SHORT).show();
+        database.noteDao().updateNote(updatedTitle, updatedDescription, notePosition);
+        Toast.makeText(NoteUpdatingActivity.this, "Update note successfully", Toast.LENGTH_SHORT).show();
         returnToNoteActivity();
     }
 
+    private void deleteNoteFromDatabase() {
+        database.noteDao().delete(new Note(notePosition,title,description));
+        Toast.makeText(NoteUpdatingActivity.this, "Delete note successfully", Toast.LENGTH_SHORT).show();
+        returnToNoteActivity();
+    }
     private void returnToNoteActivity() {
         Intent intent = new Intent(NoteUpdatingActivity.this, NoteActivity.class);
         startActivity(intent);
