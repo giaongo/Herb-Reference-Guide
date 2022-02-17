43ba323330cdc2e0df92926ae1d6a1d7c8b5176b
Tai Nguyen <phatn@metropolia.fi>
2022-02-17 18:23:22 +0200

    Create Intent in PlantActivity
    Insert hardcoded databased from collection

--- app/src/main/java/fi/metropolia/herbreferenceguide/Plant/PlantActivity.java
+++ app/src/main/java/fi/metropolia/herbreferenceguide/Plant/PlantActivity.java
@@ -10,11 +10,19 @@ import java.util.ArrayList;
 
 import fi.metropolia.herbreferenceguide.MainActivity;
 import fi.metropolia.herbreferenceguide.R;
+import fi.metropolia.herbreferenceguide.RecyclerViewInterface;
 import fi.metropolia.herbreferenceguide.database.AppDatabase;
 import fi.metropolia.herbreferenceguide.database.Plant;
 
-public class PlantActivity extends MainActivity {
-    ArrayList<Plant> plant;
+public class PlantActivity extends MainActivity implements RecyclerViewInterface {
+    ArrayList<Plant> plantList;
+    protected static final String PLANT_ID = "Plant Id";
+    protected static final String PLANT_NAME = "Plant Name";
+    protected static final String PLANT_TYPE = "Plant Type";
+    protected static final String PLANT_NUTRITIONVALUE = "Plant Nutrition Value";
+    protected static final String PLANT_HEALTHBENEFIT = "Plant Health Benefit";
+    protected static final String PLANT_FOODSUGGESTION = "Plant Food Suggestion";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,13 +36,36 @@ public class PlantActivity extends MainActivity {
         String plantType = intent.getStringExtra(TYPE);
         setTitle(plantType);
         AppDatabase database = AppDatabase.getINSTANCE(PlantActivity.this);
-        plant  = (ArrayList<Plant>) database.plantDao().getPlantByType(plantType);
+        plantList  = (ArrayList<Plant>) database.plantDao().getPlantByType(plantType);
     }
 
     private void initRecyclerView() {
         RecyclerView myVeggieRecyclerView = findViewById(R.id.veggie);
         myVeggieRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        PlantRecyclerViewAdapter plantRecyclerViewAdapter = new PlantRecyclerViewAdapter(plant,this);
+        PlantRecyclerViewAdapter plantRecyclerViewAdapter = new PlantRecyclerViewAdapter(plantList,this);
         myVeggieRecyclerView.setAdapter(plantRecyclerViewAdapter);
     }
+
+    @Override
+    public void onItemClick(int position) {
+        Intent intent = new Intent(PlantActivity.this, PlantActivity.class);
+        int plantId = plantList.get(position).getPlantId();
+        String plantName = plantList.get(position).getPlantName();
+        String plantType = plantList.get(position).getPlantType();
+        String plantNutritionValue = plantList.get(position).getPlantNutrition();
+        String plantHealthBenefit = plantList.get(position).getPlantHealthBenefit();
+        String plantFoodSuggestion = plantList.get(position).getPlantFoodSuggestion();
+        String plantImgSrc = plantList.get(position).getPlantImgSrc();
+
+        intent.putExtra(PLANT_ID,plantId);
+        intent.putExtra(PLANT_NAME, plantName);
+        intent.putExtra(PLANT_TYPE, plantType);
+        intent.putExtra(PLANT_HEALTHBENEFIT,plantHealthBenefit);
+        intent.putExtra(PLANT_NUTRITIONVALUE, plantNutritionValue);
+        intent.putExtra(PLANT_FOODSUGGESTION, plantFoodSuggestion);
+
+        startActivity(intent);
+
+    }
+
 }
