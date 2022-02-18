8748f8daf6f3c31715a6d0084c5f51d4e3a429ab
Tai Nguyen <phatn@metropolia.fi>
2022-02-18 22:05:19 +0200

    Create Intent in PlantActivity
    Insert hardcoded databased from collection
    
    Made changes for PlantActivity
    
    fixed database file
    
    updated database file
    
    Added pics to asset folder

--- app/src/main/java/fi/metropolia/herbreferenceguide/ItemDisplayActivity.java
+++ app/src/main/java/fi/metropolia/herbreferenceguide/ItemDisplayActivity.java
@@ -2,13 +2,21 @@ package fi.metropolia.herbreferenceguide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
-public class ItemDisplayActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.io.InputStream;
 
+public class ItemDisplayActivity extends AppCompatActivity {
+    private ImageView plantImg;
+    private AssetManager assetManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +28,15 @@ public class ItemDisplayActivity extends AppCompatActivity {
         fab.setOnClickListener(view -> Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show());
 
+        plantImg = findViewById(R.id.imageView_Carrot);
+        assetManager = this.getAssets();
+        InputStream is = null;
+        try {
+            is = assetManager.open("Img/Herbs/oregano.jpg");
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        Bitmap bitmap = BitmapFactory.decodeStream(is);
+        plantImg.setImageBitmap(bitmap);
     }
 }
