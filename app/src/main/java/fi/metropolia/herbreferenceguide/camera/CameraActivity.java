3ec4e0e4eb3f9e6269c78496474476620a721cbd
Giao Ngo <giao.ngo@metropolia.fi>
2022-03-01 22:24:52 +0200

    Writing Javadoc to CameraActivity. Contribute to #16.

--- app/src/main/java/fi/metropolia/herbreferenceguide/camera/CameraActivity.java
+++ app/src/main/java/fi/metropolia/herbreferenceguide/camera/CameraActivity.java
@@ -32,7 +32,8 @@ import fi.metropolia.herbreferenceguide.R;
 
 /**
  * This activity implements the multiple request permission of Camera and Media storage.
- * Displays camera preview with feature of taking, viewing photos and flipping the camera lens.
+ * Displays camera preview with feature of taking, viewing photos and flipping the camera lens with
+ * the utilisation of CameraX API
  *
  * @author Giao Ngo
  * @version 1.0
@@ -48,6 +49,15 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
     private ImageCapture imageCapture;
     public static final String LENS_FACING = "lens";
 
+    /**
+     * Launches activity for result. The permission result is recorded based on the permission result
+     * received back from user's decision (either granted permission access or denied).
+     * Code reference on the topic of request multiple permissions:
+     * @see <a href="https://www.youtube.com/watch?v=nkayHRT8D_w&ab_channel=Foxandroid">
+     * How to request multiple permissions</a>
+     * <a href="https://developer.android.com/training/permissions/requesting">
+     *  Android Studio: Request app permission</a>
+     */
     ActivityResultLauncher<String[]> mPermissionResultLauncher = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(),
             result -> {
                 if (result.containsKey(Manifest.permission.READ_EXTERNAL_STORAGE)) {
@@ -61,6 +71,11 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
                 }
             });
 
+    /**
+     * This function is used to set menu title, request multiple permissions from user and
+     * activates camera after all permission is requested.
+     * @param savedInstanceState Bundle
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -73,24 +88,37 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
         (findViewById(R.id.btnFlipCamera)).setOnClickListener(this);
     }
 
+    /**
+     * Requests permission from user in regard of read external storage, write external storage and
+     * camera.
+     * Code reference on the topic of request multiple permissions:
+     * @see <a href="https://www.youtube.com/watch?v=nkayHRT8D_w&ab_channel=Foxandroid">
+     * How to request multiple permissions</a>
+     * <a href="https://developer.android.com/training/permissions/requesting">
+     * Android Studio: Request app permission</a>
+     */
     private void requestPermission() {
+        //Check if every permission is granted from the beginning
         isReadPermissionGranted = ContextCompat.checkSelfPermission(
                 this,
                 Manifest.permission.READ_EXTERNAL_STORAGE
         ) == PackageManager.PERMISSION_GRANTED;
 
-        isWritePermissionGranted = ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-        ) == PackageManager.PERMISSION_GRANTED;
-
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            isWritePermissionGranted = ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED;
+        } else {
+            isWritePermissionGranted = true;
+        }
         isCameraPermissionGranted = ContextCompat.checkSelfPermission(
                 this,
                 Manifest.permission.CAMERA
         ) == PackageManager.PERMISSION_GRANTED;
 
+        //In the case permission is not granted, add the permission to ArrayList and launch permission request for each
         ArrayList<String> permissionRequest = new ArrayList<>();
-
         if (!isReadPermissionGranted) {
             permissionRequest.add(Manifest.permission.READ_EXTERNAL_STORAGE);
         }
@@ -105,8 +133,16 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
         }
     }
 
+    /**
+     * Requests a CameraProvider and check for CameraProvider availability
+     * Code reference of CameraX api:
+     * @see <a href="https://developer.android.com/training/camerax/preview">
+     * Implement a preview: Request a camera provider</a>
+     */
     private void initCamera() {
+        // request a camera provider
         cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        // verify that the initialization succeeded when the view is created
         cameraProviderFuture.addListener(() -> {
             try {
                 ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
@@ -117,38 +153,62 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
         }, ContextCompat.getMainExecutor(this));
     }
 
+    /**
+     * Select a camera and bind the lifecycle and use cases
+     * @param cameraProvider ProcessCameraProvider
+     * @see <a href="https://developer.android.com/training/camerax/preview">
+     * Implement a preview: Request a camera provider</a>
+     */
     private void bindPreview(ProcessCameraProvider cameraProvider) {
+        // unbind all bound use cases
         cameraProvider.unbindAll();
+        // create a Preview
         previewView = findViewById(R.id.viewCamera);
         Preview preview = new Preview.Builder().build();
+        /* specify camera lens facing option (lens facing is back by default but user can change by
+         clicking on flipping button) */
         CameraSelector cameraSelector = new CameraSelector.Builder()
                 .requireLensFacing(lensFacing)
                 .build();
+        // provide a surface for Preview
         preview.setSurfaceProvider(previewView.getSurfaceProvider());
         imageCapture = new ImageCapture.Builder()
                 .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
                 .build();
+        // Bind the selected camera and any use cases to lifecycle
         cameraProvider.bindToLifecycle(this, cameraSelector, imageCapture, preview);
     }
 
+    /**
+     * Event click listener for each button
+     * @param view View
+     */
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.btnTakePhoto) {
             capturePhoto();
-        } else if(view.getId() == R.id.btnViewPhoto) {
+        } else if (view.getId() == R.id.btnViewPhoto) {
             viewPhoto();
         } else {
             flipCameraLens();
         }
     }
 
+    /**
+     * Captures photo and saves the images to shared MediaStore.
+     * Code reference:
+     * @see <a href="https://developer.android.com/training/camerax/take-photo">Image capture: Implementation</a>
+     * <a href="https://developer.android.com/reference/androidx/camera/core/ImageCapture.OutputFileOptions.Builder">
+     *  ImageCapture.OutputFileOptions.Builder: Public constructors</a>
+     */
     private void capturePhoto() {
-        // Creating image display name in external storage
+        // Creating image display name in external storage starting with herb and current date
         Date date = Calendar.getInstance().getTime();
         DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.SHORT);
         String strDate = dateFormat.format(date);
         String displayName = "herb" + strDate + ".jpg";
 
+        // Sets the values of each column and inserts the data to that column
         ContentValues contentValues = new ContentValues();
         contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, displayName);
         contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
@@ -158,6 +218,8 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
         } else {
             imageCollection = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
         }
+
+        // Takes picture and saves the images output to MediaStore
         ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions
                 .Builder(
                 getContentResolver(),
@@ -170,7 +232,6 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
                         Toast.makeText(CameraActivity.this, "Image Saved Successfully",
                                 Toast.LENGTH_SHORT).show();
                     }
-
                     @Override
                     public void onError(@NonNull ImageCaptureException exception) {
                         Toast.makeText(CameraActivity.this, "Error with saving images",
@@ -179,12 +240,18 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
                 });
     }
 
+    /**
+     * Launches ImageGalleryActivity
+     */
     private void viewPhoto() {
         Intent intent = new Intent(CameraActivity.this, ImageGalleryActivity.class);
         intent.putExtra(LENS_FACING, lensFacing);
         startActivity(intent);
     }
 
+    /**
+     * Flips front or back camera lens and re-initialise camera to update with the lens-facing
+     */
     private void flipCameraLens() {
         if (lensFacing == CameraSelector.LENS_FACING_BACK) {
             lensFacing = CameraSelector.LENS_FACING_FRONT;
