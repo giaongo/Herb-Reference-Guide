da9fdf00dd2c8968b6d89f38230cd10421be805b
Giao Ngo <giao.ngo@metropolia.fi>
2022-03-02 10:30:33 +0200

    Writing Javadoc to Interface and add gradle. Contribute to #16.

--- app/src/main/java/fi/metropolia/herbreferenceguide/RecyclerViewInterface.java
+++ app/src/main/java/fi/metropolia/herbreferenceguide/RecyclerViewInterface.java
@@ -1,5 +1,8 @@
 package fi.metropolia.herbreferenceguide;
 
+/**
+ * Defines interface on RecyclerView item clicked
+ */
 public interface RecyclerViewInterface {
     void onItemClick(int position);
 
